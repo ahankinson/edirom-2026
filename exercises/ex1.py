efb811b285ce05bce2c21c0536859ba76c0b927d
Andrew Hankinson <andrew.hankinson@gmail.com>
2026-09-18 09:44:31 +0000

    Changes from the class

--- exercises/ex1.py
+++ exercises/ex1.py
@@ -2,7 +2,11 @@
 # Run it with `python exercises/ex1.py` in the terminal, or with the "|>" button in the top-right.
 # Your solution must contain at least one function.
 
+def print_sentence(name: str):
+    print(f"<{name} />")
+
 
 if __name__ == "__main__":
     # Hint: Once you have defined a function, remove the pass statement and call the function here.
-    pass
+    print_sentence("note")
+    print_sentence("measure")
--- exercises/ex2.py
+++ exercises/ex2.py
@@ -2,5 +2,8 @@
 # Your solution must contain only one print() statement
 # Run it with `python exercises/ex2.py` in the terminal.
 
+def is_leap_year(year: int) -> bool:
+    return (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0)
+
 if __name__ == "__main__":
-    pass
+    print(is_leap_year(1200))
